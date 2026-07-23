273ca2b9c891513586362be9ee1b4a78bf989a5b
Sullivan Amadike <amadikesullivan20@gmail.com>
2026-07-23 01:57:37 +0100

    made password optional field

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -15,7 +15,7 @@ export class User {
     @Prop({ required: true, unique: true, index: true })
     email: string
 
-    @Prop({ required: true, select: false, default: null })
+    @Prop({ select: false, default: null })
     password: string
 
     @Prop({ required: true })
